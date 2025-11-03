5eb0716f135e203018877df213d300690e4f0e45
Roman Volodin <me@romanvolodin.com>
2025-11-02 21:26:30 -0800

    removed Hello World command

--- src/extension.ts
+++ src/extension.ts
@@ -10,16 +10,6 @@ export function activate(context: vscode.ExtensionContext) {
 	// This line of code will only be executed once when your extension is activated
 	console.log('Congratulations, your extension "t2py" is now active!');
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	const helloCmd = vscode.commands.registerCommand('t2py.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from t2py!');
-	});
-
-
 	// --- Command Convert List ---
 	const convertCmd = vscode.commands.registerCommand('t2py.convertList', async () => {
 		const editor = vscode.window.activeTextEditor;
@@ -44,7 +34,7 @@ export function activate(context: vscode.ExtensionContext) {
 		await editor.insertSnippet(snippet, rangeToProcess);
 	});
 
-	context.subscriptions.push(helloCmd, convertCmd);
+	context.subscriptions.push( convertCmd);
 }
 
 // This method is called when your extension is deactivated
